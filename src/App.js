8ffd231994668359a1ff39026f3ae540387084ef
pinkphi <pamela.u.a@gmail.com>
2021-12-06 21:39:36 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logoshecodes.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img  src={logo} className="App-logo img-fluid"
+      alt="logo" />
+     </header> 
+    </div>
+  );
+}
+
+export default App;
