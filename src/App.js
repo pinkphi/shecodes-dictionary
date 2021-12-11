cd9f41af7136d581fd856c2b56f9508fe6c1a27c
pinkphi <pamela.u.a@gmail.com>
2021-12-10 21:45:38 -0500

    minor fixes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-      <img  src={logo} className="App-logo img-fluid"
-      alt="logo" />
+      <img  src={logo} className="App-logo img-fluid" alt="logo" />
      </header>
      <main>
        <Dictionary />
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,33 +4,31 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    const [keyword, setKeyword] = useState("");
-    const [results, setResults] = useState(null);
-    
-function handleResponse(response) {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
     setResults(response.data[0]);
-}
+  }
 
-    function search(event) {
-        event.preventDefault();
-        
-        // documentation: https://dictionaryapi.dev/
+  function search(event) {
+    event.preventDefault();
 
-        let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
-    }
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function handleKeywordChange(event) {
-      setKeyword(event.target.value);  
-    }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-    return (
+  return (
     <div className="Dictionary">
-        <form onSubmit={search}>
-            <input type="search" onChange=
-            {handleKeywordChange} />
-        </form>
-        <Results resutls={results} />
-        </div>
-    );
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
